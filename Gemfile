92ae8779a1d36dd34a9723a6b2b2dc75130751ba
dzhiqin <dzhiqin@126.com>
2017-07-04 15:53:09 +0800

    implement posts pagination with will_paginate

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'coffee-rails', '~> 4.2'
 gem 'bootstrap-sass'
 gem 'simple_form'
 gem 'devise'
+gem 'will_paginate'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
@@ -52,8 +53,8 @@ group :development do
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '~> 3.0.5'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
-  # gem 'spring'
-  # gem 'spring-watcher-listen', '~> 2.0.0'
+   gem 'spring'
+   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!,:only =>[:new,:create]
+
+  def new
+    @group=Group.find(params[:group_id])
+    @post=Post.new
+  end
+
+  def create
+    @group=Group.find(params[:group_id])
+    @post=Post.new(post_params)
+    @post.group=@group
+    @post.user=current_user
+
+    if @post.save
+      redirect_to group_path(@group)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post=Post.find(params[:id])
+  end
+
+  def update
+    @group=Group.find(params[:group_id])
+    @post=Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to group_path(@group)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group=Group.find(params[:group_id])
+    @post=Post.fid(params[:id])
+    @post.destroy
+    redirect_to group_path(@group)
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+end
